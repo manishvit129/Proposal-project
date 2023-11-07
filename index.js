2081977be57d57cefb4b511d359e233561dd2aad
manishvit129 <150160394+manishvit129@users.noreply.github.com>
2023-11-07 21:55:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+    const loveButton = document.getElementById("loveButton");
+    const loveMessage = document.getElementById("loveMessage");
+
+    loveButton.style.display = "none";
+    loveMessage.style.display = "block";
+});
